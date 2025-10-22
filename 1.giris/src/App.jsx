a1513e6aa65d24c96eb41483a436552ba212661a
Hanife Alay <hanifealay@Hanife-MacBook-Air.local>
2025-10-22 13:35:20 +0300

    React first  commit

---
+++ 1.giris/src/App.jsx
@@ -0,0 +1,18 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  // jsx: "{ }" jsx dediğmiz yapı bu süslü parantezlerin içi oluyor aslında 
+  // Buraya JavaScrip kodları yazılır.
+
+  return (
+    // HTML Kodalrı yazılır.
+    <div>
+      Hanife 
+    </div>
+  )
+}
+
+export default App
