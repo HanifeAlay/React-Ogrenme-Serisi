d616505e0fe9d80dd4ea1c77be6231e2d3b92b67
Hanife Alay <hanifealay@Hanife-MacBook-Air.local>
2025-10-22 15:45:58 +0300

    Day 1: JSX, key, map ve ternary konuları eklendi

--- 1.giris/src/App.jsx
+++ 1.giris/src/App.jsx
@@ -4,13 +4,33 @@ import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  // jsx: "{ }" jsx dediğmiz yapı bu süslü parantezlerin içi oluyor aslında 
+  // jsx: "{ }" jsx dediğmiz yapı bu süslü parantezlerin içi oluyor aslında. JS ve HTML kodununu birleştirir bu süslü parantezler.
   // Buraya JavaScrip kodları yazılır.
+  //let a = 15;
+ 
+  let vize1 = 60;
+  let vize2 = 40;
+
+  let sonuc = false;
+
+  let isimler = [
+   "Ahmet",
+   "Ayşe",
+   "Fatma",
+   "Ali"
+  ]
 
   return (
     // HTML Kodalrı yazılır.
     <div>
-      Hanife 
+     
+     {
+     isimler .map((isim, index) =>(
+     <div key={index} style={{backgroundColor: "pink"}}> 
+      {isim}
+     </div>
+     ))
+     }
     </div>
   )
 }
