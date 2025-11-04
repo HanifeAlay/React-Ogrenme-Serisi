e0be002270cb73f86e3b2217dedf4ba3e1d35df5
Hanife Alay <hanifealay@Hanife-MacBook-Air.local>
2025-11-04 17:38:20 +0300

    4-5 Days

---
+++ 8.api-using/src/App.jsx
@@ -0,0 +1,50 @@
+import { useEffect, useState } from 'react'
+import './App.css'
+import axios from 'axios'
+
+const BASE_URL = "http://localhost:3000";
+
+function App() {
+  const getAllUsers = async()=>{
+   const response =  await axios.get(`${BASE_URL}/users`);
+   console.log(response.data)
+    }
+
+    const getUserById = async (userId) => {
+      const response = await axios.get (`${BASE_URL}/users/${userId}`);
+      console.log(response.data)
+    }
+
+    const createUser = async (newUser) => {
+      //Post : veri eklmek için kullanılır.
+     const response =  await axios.post (`${BASE_URL}/users`,newUser);
+      console.log("response", response.data);
+    }
+
+    // bilgi güncelleme işlemi
+    const updateUser = async (userId, updatedUser) => {
+      // put : veri güncellemek için kullanılır.
+     await axios.put(`${BASE_URL}/users/${userId}`, updatedUser);
+    }
+
+
+    useEffect (()=>{
+      // const newUser = {
+      //  "username" : "Yusuf",
+       // "password" : "6767"
+      //}
+      //createUser(newUser)
+      updateUser("5631", {
+        "username" : "Muhammed",
+        "password" : "1212"
+      })
+    },[])
+
+  return (
+    <div>
+      hanife
+    </div>
+  )
+}
+
+export default App
